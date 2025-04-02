275632643effdbecd2fcaced5bd8a01c2e5259bf
robkenha3 <robkenha@gmail.com>
2025-04-01 22:09:36 -0300

    Estruturado o layout da página modo Pokédex

--- src/pages/PokedexMode.js
+++ src/pages/PokedexMode.js
@@ -1,11 +1,29 @@
 import React from 'react'
 import Pokedex from '../assets/Images/Pokédex.jpg'
 
+const ButtonEffect = () => {
+    
+}
+
+
 const PokedexMode = () => {
   return (
     <div className='PokedexMode'>
         <div className='PokedexMode__container'>
-            <img src={Pokedex} alt="Imagem da Pokédex"></img>
+
+          <div className = "PokedexMode__background"></div>
+          
+          <img style={{position: "relative", zIndex: 1}} src={Pokedex} alt="Imagem da Pokédex"></img>
+
+          <div className="PokedexMode__button">
+            <button onClick={() => {}}></button>
+            <button onClick={() => {}}></button>
+          </div>
+
+          <div className="PokedexMode__circle-button">
+            <button onClick={() => {}}></button>
+          </div>
+          
         </div>
     </div>
   )
