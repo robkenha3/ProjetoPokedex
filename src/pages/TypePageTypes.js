65bef9fc31bf372e887305c29e23f6dd2596f93b
robkenha3 <robkenha@gmail.com>
2025-04-23 00:48:08 -0300

    Atualizado página de tipos específicos

--- src/pages/TypePageTypes.js
+++ src/pages/TypePageTypes.js
@@ -3,6 +3,7 @@ import { ItemListPokemonFilter } from '../components/ItemListPokemonFilter.js'
 import { arrayPokemon } from '../assets/database/pokemon151.js'
 import { arrayTypes } from '../assets/database/arrayTypes.js'
 import { useParams } from 'react-router'
+import Type from '../components/Type.js'
 
 const TypePageTypes = () => {
   const currentType = useParams();
@@ -11,12 +12,104 @@ const TypePageTypes = () => {
   // const pokemonCurrentType = pokemonTypes.filter((el,i) => {return el[i] === currentType})
   // console.log(pokemonCurrentType)
 
+  const pageType = arrayTypes.find(el => el.type === currentType.tipo);
+
   return (
     <div className='TypePageTypes'>
         <div className="TypePageTypes__container">
           <div className='TypePageTypes__description'>
-            <h1>Ataque <b>pros & cons</b></h1>
-            <h2>Movimentos do tipo {currentType.tipo} não são efetivos contra</h2>
+            <h1>Ataque <span>pros & cons</span></h1>
+            <div className='TypePageTypes__description-atk'>
+              {pageType.attackProsCons?.notVeryEffective 
+                ? 
+                <div>
+                  <h2>Movimento do tipo {currentType.tipo} não são efetivos contra:</h2>
+                  <div>
+                    {pageType.attackProsCons.notVeryEffective.map(el => {
+                      return <Type type={el}/>  
+                    })}
+                  </div>
+                  <br/>
+                  <br/>
+                </div>
+                :
+                <div></div>
+              }
+
+              {pageType.attackProsCons?.superEffective 
+                ? 
+                <div>
+                  <h2>Movimento do tipo {currentType.tipo} são super efetivos contra:</h2>
+                  <div>
+                    {pageType.attackProsCons.superEffective.map(el => {
+                      return <Type type={el}/>
+                    })}
+                  </div>
+                  <br/>
+                  <br/>
+                </div>
+                :
+                <div></div>
+              }
+
+              {pageType.attackProsCons?.noEffect 
+                ?
+                <div>
+                  <h2>Movimento do tipo {currentType.tipo} não tem efeito contra:</h2>
+                  {pageType.attackProsCons.noEffect.map(el => {
+                    return <Type type={el}/>
+                  })}
+                </div> 
+                :
+                <div></div>
+              }
+            </div>
+
+            <h1>Defesa <span>pros & cons</span></h1>
+            <div className='TypePageTypes__description-def'>
+              {pageType.defenseProsCons?.noEffect
+                ?
+                <div>
+                  <h2>Esses tipos não tem efeito contra Pokémon do tipo {currentType.tipo}:</h2>
+                  {pageType.defenseProsCons.noEffect.map(el => {
+                    return <Type type={el}/>
+                  })}
+                  <br/>
+                  <br/>
+                </div>
+                :
+                <div></div>
+              }
+
+              {pageType.defenseProsCons?.notVeryEffective
+                ?
+                <div>
+                  <h2>Esses tipos não são efetivos contra Pokémon do tipo {currentType.tipo}:</h2>
+                  {pageType.defenseProsCons.notVeryEffective.map(el => {
+                    return <Type type={el}/>
+                  })}
+                  <br/>
+                  <br/>
+                </div>
+                :
+                <div></div>
+              }
+
+              {pageType.defenseProsCons?.superEffective
+                ?
+                <div>
+                  <h2>Esses tipos são super efetivos contra Pokémon do tipo {currentType.tipo}:</h2>
+                  {pageType.defenseProsCons.superEffective.map(el => {
+                    return <Type type={el}/>
+                  })}
+                </div>
+                :
+                <div></div>
+              }
+
+            </div>
+            
+
           </div>
 
           
